58fa8e02bafc09061d70acf2b3095984b6cc91da
이경용 <sleekydz86@icloud.com>
2023-12-14 21:16:10 +0900

    RinggoApplication 테스트 클래스 생성
    RingconApplication 테스트 클래스 생성

--- src/main/java/com/ringo/logins/ringologin/RingoLoginApplication.java
+++ src/main/java/com/ringo/logins/ringologin/RingoLoginApplication.java
@@ -2,12 +2,21 @@ package com.ringo.logins.ringologin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
 
 @SpringBootApplication
+@EnableFeignClients
+@EnableConfigurationProperties
 public class RingoLoginApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RingoLoginApplication.class, args);
     }
 
+
+
 }
+
+
---
+++ src/test/java/com/ringo/logins/ringologin/AppleAuthClientTest.java
@@ -0,0 +1,9 @@
+package com.ringo.logins.ringologin;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("default")
+public class AppleAuthClientTest {
+}
