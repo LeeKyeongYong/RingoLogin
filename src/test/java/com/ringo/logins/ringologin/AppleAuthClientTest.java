054108ae9390f10b62f8b6c688ba2457ca7ba4d5
이경용 <sleekydz86@icloud.com>
2023-12-15 23:35:56 +0900

    1
    1

--- src/test/java/com/ringo/logins/ringologin/AppleAuthClientTest.java
+++ src/test/java/com/ringo/logins/ringologin/AppleAuthClientTest.java
@@ -1,9 +1,5 @@
 package com.ringo.logins.ringologin;
 
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-
-@SpringBootTest
-@ActiveProfiles("default")
 public class AppleAuthClientTest {
+    int a;
 }
